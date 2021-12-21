876b5d8f4c70aa3d5886b743ff503958f881f889
thl <lassen.thomas@gmail.com>
2021-12-21 22:45:51 +0100

    Boost dir and Boost serialization experiments
    Trying to figure out why friend class boost::serialization::access; sometimes fails when in other than global namespace.

--- hello-exe/main.cpp
+++ hello-exe/main.cpp
@@ -1,56 +1,270 @@
-#pragma region OLD MAIN
+#include <fstream>
 #include <iostream>
-#include "src1.h"
-#include "src2_lib2.h"
-#include "src_lib3.h"
-#include <say-hello/hello.h>
-#include <boost/lambda/lambda.hpp>
-#include <boost/timer/timer.hpp>
-#include <iterator>
-#include <algorithm>
-#include <boost-hello/boost-hello.hpp>
 
-using namespace std;
+// include headers that implement a archive in simple text format
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
-int main()
+/////////////////////////////////////////////////////////////
+// gps coordinate
+//
+// illustrates serialization for a simple type
+//
+namespace boost_ser{
+class gps_position
 {
-    cout << helloboost() << endl;
-    cout << "Hello World!" << endl;
-
-    cout << lib1::hello_world() << endl;
-
-    lib2::src2_lib2 foo{"CMake test"};
-
-    cout << foo.my_name_is() << endl;
-
-
-    lib3::person p{"Bibi", 0};
-
-    cout << "Name: " << p.my_name_is() << " and age is " << p.get_age() << endl;
-
-    hello::say_hello();
-
-    std::cout << "say-hello version is " << SAY_HELLO_VERSION << std::endl;
-
-    // using namespace boost::lambda;
-    // typedef std::istream_iterator<int> in;
-
-    // std::for_each(
-    //     in(std::cin), in(), std::cout << (_1 * 3) << " " );
-
-    boost::timer::cpu_timer aclock{};
-
-
-    size_t j = 0;
-    for (size_t i = 0; i < 1000000000; ++i)
+private:
+    friend std::ostream & operator<<(std::ostream &os, const gps_position &gp);
+    friend class boost::serialization::access;
+    // When the class Archive corresponds to an output archive, the
+    // & operator is defined similar to <<.  Likewise, when the class Archive
+    // is a type of input archive the & operator is defined similar to >>.
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
     {
-        j = i * 2;
+        ar & degrees;
+        ar & minutes;
+        ar & seconds;
     }
-    
-    aclock.stop();
+    int degrees;
+    int minutes;
+    float seconds;
+public:
+    gps_position(){};
+    gps_position(int d, int m, float s) :
+        degrees(d), minutes(m), seconds(s)
+    {}
+};
 
-    std::cout << "Elapsed time " << aclock.format() << std::endl;
-    std::cout << j << std::endl;
+
+
+std::ostream & operator<<(std::ostream &os, const gps_position &gp)
+{
+    return os << ' ' << gp.degrees << (unsigned char)186 << gp.minutes << '\'' << gp.seconds << '"';
+}
+
+/////////////////////////////////////////////////////////////
+// One bus stop
+//
+// illustrates serialization of serializable members
+//
+
+class bus_stop
+{
+    friend class boost::serialization::access;
+    friend std::ostream & operator<<(std::ostream &os, const bus_stop &gp);
+    virtual std::string description() const = 0;
+    gps_position latitude;
+    gps_position longitude;
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & latitude;
+        ar & longitude;
+    }
+protected:
+    bus_stop(const gps_position & _lat, const gps_position & _long) :
+        latitude(_lat), longitude(_long)
+    {}
+public:
+    bus_stop(){}
+    virtual ~bus_stop(){}
+};
+
+BOOST_SERIALIZATION_ASSUME_ABSTRACT(bus_stop)
+
+std::ostream & operator<<(std::ostream &os, const bus_stop &bs)
+{
+    return os << bs.latitude << bs.longitude << ' ' << bs.description();
+}
+
+/////////////////////////////////////////////////////////////
+// Several kinds of bus stops
+//
+// illustrates serialization of derived types
+//
+class bus_stop_corner : public bus_stop
+{
+    friend class boost::serialization::access;
+    std::string street1;
+    std::string street2;
+    virtual std::string description() const
+    {
+        return street1 + " and " + street2;
+    }
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        // save/load base class information
+        ar & boost::serialization::base_object<bus_stop>(*this);
+        ar & street1 & street2;
+    }
+
+public:
+    bus_stop_corner(){}
+    bus_stop_corner(const gps_position & _lat, const gps_position & _long,
+        const std::string & _s1, const std::string & _s2
+    ) :
+        bus_stop(_lat, _long), street1(_s1), street2(_s2)
+    {
+    }
+};
+
+class bus_stop_destination : public bus_stop
+{
+    friend class boost::serialization::access;
+    std::string name;
+    virtual std::string description() const
+    {
+        return name;
+    }
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & boost::serialization::base_object<bus_stop>(*this) & name;
+    }
+public:
+
+    bus_stop_destination(){}
+    bus_stop_destination(
+        const gps_position & _lat, const gps_position & _long, const std::string & _name
+    ) :
+        bus_stop(_lat, _long), name(_name)
+    {
+    }
+};
+
+/////////////////////////////////////////////////////////////
+// a bus route is a collection of bus stops
+//
+// illustrates serialization of STL collection templates.
+//
+// illustrates serialzation of polymorphic pointer (bus stop *);
+//
+// illustrates storage and recovery of shared pointers is correct
+// and efficient.  That is objects pointed to by more than one
+// pointer are stored only once.  In such cases only one such
+// object is restored and pointers are restored to point to it
+//
+class bus_route
+{
+    friend class boost::serialization::access;
+    friend std::ostream & operator<<(std::ostream &os, const bus_route &br);
+    typedef bus_stop * bus_stop_pointer;
+    std::list<bus_stop_pointer> stops;
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        // in this program, these classes are never serialized directly but rather
+        // through a pointer to the base class bus_stop. So we need a way to be
+        // sure that the archive contains information about these derived classes.
+        //ar.template register_type<bus_stop_corner>();
+        ar.register_type(static_cast<bus_stop_corner *>(NULL));
+        //ar.template register_type<bus_stop_destination>();
+        ar.register_type(static_cast<bus_stop_destination *>(NULL));
+        // serialization of stl collections is already defined
+        // in the header
+        ar & stops;
+    }
+public:
+    bus_route(){}
+    void append(bus_stop *_bs)
+    {
+        stops.insert(stops.end(), _bs);
+    }
+};
+std::ostream & operator<<(std::ostream &os, const bus_route &br)
+{
+    std::list<bus_stop *>::const_iterator it;
+    // note: we're displaying the pointer to permit verification
+    // that duplicated pointers are properly restored.
+    for(it = br.stops.begin(); it != br.stops.end(); it++){
+        os << '\n' << std::hex << "0x" << *it << std::dec << ' ' << **it;
+    }
+    return os;
+}
+}
+
+using namespace boost_ser;
+
+int main() {
+    // create and open a character archive for output
+    std::ofstream ofs("filename");
+
+    using namespace boost_ser;
+    // create class instance
+    const boost_ser::gps_position g(35, 59, 24.567f);
+
+    std::cout << g << std::endl;
+
+    // save data to archive
+    {
+        boost::archive::text_oarchive oa(ofs);
+        // write class instance to archive
+        oa << g;
+    	// archive and stream closed when destructors are called
+    }
+
+    // ... some time later restore the class instance to its orginal state
+    gps_position newg;
+    {
+        // create and open an archive for input
+        std::ifstream ifs("filename");
+        boost::archive::text_iarchive ia(ifs);
+        // read class state from archive
+        ia >> newg;
+        // archive and stream closed when destructors are called
+    }
+
+    
+    // fill in the data
+    // make a few stops
+    std::unique_ptr<bus_stop> bs0 {std::make_unique<bus_stop_corner>(
+        gps_position(34, 135, 52.560f),
+        gps_position(134, 22, 78.30f),
+        "24th Street", "10th Avenue"
+    )};
+
+    bus_stop *bs1 = new bus_stop_corner(
+        gps_position(35, 137, 23.456f),
+        gps_position(133, 35, 54.12f),
+        "State street", "Cathedral Vista Lane"
+    );
+    bus_stop *bs2 = new bus_stop_destination(
+        gps_position(35, 136, 15.456f),
+        gps_position(133, 32, 15.300f),
+        "White House"
+    );
+    bus_stop *bs3 = new bus_stop_destination(
+        gps_position(35, 134, 48.789f),
+        gps_position(133, 32, 16.230f),
+        "Lincoln Memorial"
+    );
+
+    std::cout << *bs0 << std::endl;
+    std::cout << *bs1 << std::endl;
+    std::cout << *bs2 << std::endl;
+    std::cout << *bs3 << std::endl;
+
+     // make a  routes
+    bus_route route0;
+    route0.append(bs0.get());
+    route0.append(bs1);
+    route0.append(bs2);
+
+     // make aother routes
+    bus_route route1;
+    route1.append(bs3);
+    route1.append(bs2);
+    route1.append(bs1);
+
+    std::cout << "displaying route0: " << route0 << std::endl << std::endl;
+    std::cout << "displaying route1: " << route1 << std::endl << std::endl;
+
+
+    delete bs1;
+    delete bs2;
+    delete bs3;
+
     return 0;
 }
-#pragma endregion
